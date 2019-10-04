a7aa58b64dab688e3a86e0dc30f1b9675f073bfd
Austin Gabe Scott <austin.g.scott-1@ou.edu>
2019-10-03 21:46:46 -0500

    created server.c and storage_remote.c

---
+++ server.c
@@ -0,0 +1,8 @@
+//
+//  server.c
+//  Project2
+//
+//  Created by Gabe Scott on 10/3/19.
+//  
+
+#include <stdio.h>
---
+++ storage_remote.c
@@ -0,0 +1,8 @@
+//
+//  storage_remote.c
+//  Project2
+//
+//  Created by Gabe Scott on 10/3/19.
+//
+
+#include <stdio.h>
