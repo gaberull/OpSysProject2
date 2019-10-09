500e22e8213a376552d45b16bdc1ac5542f8c748
gabescott10 <austin.g.scott-1@ou.edu>
2019-10-08 22:31:08 -0600

    added some error checking messages

--- server.c
+++ server.c
@@ -24,14 +24,17 @@ int main(int argc, char** argv)
         // Open to_storage pipe
         if ((fd_in = open("pipe_in", O_RDONLY) < 0))
         {
+            printf("exited on opening pipe_in");
             exit(-1);       // TODO: instructions say okay to terminate on failure
         }
         if ((fd_out = open("pipe_out", O_WRONLY) < 0))
         {
+            printf("exited on opening pipe_out");
             exit(-1);
         }
         if (read(fd_in, &header, sizeof(HEADER) != sizeof(HEADER) ))
         {
+            printf("exited on read of fd_in");
             exit(-1);
         }
         if (header.type == INIT_CONNECTION) // TODO: check all this possible scrap it
@@ -43,6 +46,7 @@ int main(int argc, char** argv)
             header_out.len_buffer = -1;
             if (write(fd_out, &header_out, sizeof(HEADER)) != sizeof(HEADER))
             {
+                printf("exited on write to fd_out");
                 exit (-1);
             }
             
@@ -51,6 +55,7 @@ int main(int argc, char** argv)
             char filename[30];
             if (read(fd_in, filename, size) != size)        // get filename from another read
             {
+                printf("exited on read of fd_in");
                 exit(-1);
             }
             // open file using storage.c function
@@ -59,6 +64,7 @@ int main(int argc, char** argv)
             // send aknowledge again after opening file
             if(write(fd_out, &header_out, sizeof(HEADER)) != sizeof(HEADER))
             {
+                printf("exited on write of fd_out");
                 exit(-1);
             }
             
