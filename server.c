2e719acb13cd4aa0fc88c96cfa76a611f40a6582
gabescott10 <austin.g.scott-1@ou.edu>
2019-10-11 15:23:37 -0600

    updated some perror messages in server.c

--- server.c
+++ server.c
@@ -24,18 +24,18 @@ int main(int argc, char** argv)
         // Open pipes
         if ((fd_in = open("pipe_in", O_RDONLY) < 0))
         {
-            perror("the following error occured: ");
+            perror("the following error occured while opening pipe_in: ");
             exit(-1);       // instructions say okay to terminate on failure
         }
         if ((fd_out = open("pipe_out", O_WRONLY) < 0))
         {
-            perror("exited on opening pipe_out: ");
+            perror("error while opening pipe_out: ");
             exit(-1);
         }
         // Do first read() from pipe
         if (read(fd_in, &header, sizeof(HEADER) != sizeof(HEADER) ))
         {
-            perror("exited on read of fd_in: ");
+            perror("error during read of fd_in: ");
             exit(-1);
         }
         if (header.type == INIT_CONNECTION) // check that header type is INIT 
@@ -48,7 +48,7 @@ int main(int argc, char** argv)
             // write to fd_out to send header_out back across pipe
             if (write(fd_out, &header_out, sizeof(HEADER)) != sizeof(HEADER))
             {
-                perror("exited on write to fd_out");
+                perror("error during write to fd_out: ");
                 exit (-1);
             }
             
@@ -57,7 +57,7 @@ int main(int argc, char** argv)
             char filename[size];    // create buffer to hold filename
             if (read(fd_in, filename, size) != size)        // get filename from another read
             {
-                perror("exited on read of fd_in");
+                perror("error at read of fd_in: ");
                 exit(-1);
             }
             // open (initialize) file using storage.c function
@@ -66,7 +66,7 @@ int main(int argc, char** argv)
                 // send back same aknowledge again after opening file
             if(write(fd_out, &header_out, sizeof(HEADER)) != sizeof(HEADER))
             {
-                perror("exited on write of fd_out");
+                perror("error at write of fd_out: ");
                 exit(-1);
             }
             
@@ -74,7 +74,7 @@ int main(int argc, char** argv)
         // do next read() to get next message
         if (read(fd_in, &header, sizeof(HEADER) != sizeof(HEADER) ))
         {
-            perror("read failed");
+            perror("error during read of fd_in: ");
             exit (-1);
         }
         if (header.type == READ_REQUEST)
