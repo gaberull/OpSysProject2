f11fb888588d7ec6f90a887c9b9536eb0cdb7c2b
gabescott10 <43218090+gabescott10@users.noreply.github.com>
2019-10-13 14:19:49 -0500

    Making edits on Xcode

--- server.c
+++ server.c
@@ -39,19 +39,20 @@ int main(int argc, char** argv)
             exit(-1);
         }
         // conditionals for all possible header types
-        if (header.type == INIT_CONNECTION) // check that header type is INIT 
+        if (header.type == INIT_CONNECTION) // check that header type is INIT
         {
             // prepare to Send back aknowledge
             header_out.type = ACKNOWLEDGE;
             header_out.len_message = 0;
             header_out.location = -1;
             header_out.len_buffer = -1;
+                                            /*      //removing one write
             // write to fd_out to send header_out back across pipe
             if (write(fd_out, &header_out, sizeof(HEADER)) != sizeof(HEADER))
             {
                 perror("error during write to fd_out: ");
                 exit (-1);
-            }
+            }                               */
             
             // read in again to get name of file
             int size = header.len_message;      // size of message to follow
@@ -62,9 +63,9 @@ int main(int argc, char** argv)
                 exit(-1);
             }
             // open (initialize) file using storage.c function
-            storage = init_storage(filename); 
+            storage = init_storage(filename);
             
-                // send back aknowledge after opening file
+            // send back aknowledge after opening file
             if(write(fd_out, &header_out, sizeof(HEADER)) != sizeof(HEADER))
             {
                 perror("error at write of fd_out: ");
@@ -74,17 +75,17 @@ int main(int argc, char** argv)
         else if (header.type == READ_REQUEST)
         {
             // client is requesting DATA.
-            // set return header message           
+            // set return header message
             header_out.type = DATA;
             header_out.len_message = 0;
             header_out.location = header.location;
-            //header_out.len_buffer = header.len_buffer;              
-
+            //header_out.len_buffer = header.len_buffer;
+            
             // get_bytes() from storage.c
             ret = get_bytes(storage, buffer, header.location, header.len_buffer);
             // TODO: if ret == 0, EOF, if -1, error I think. maybe send this back in header message
             header_out.len_buffer = ret;
-
+            header_out.len_message = ret;
             // send back the bytes read to the buffer
             // ret will have the number of bytes retreived or 0 for EOF
             write(fd_out, buffer, ret);     //TODO: maybe add error check
@@ -99,23 +100,29 @@ int main(int argc, char** argv)
             
             // get the buffer from the remote to be written
             read(fd_in, buffer, header.len_buffer);
-
+            
             ret = put_bytes(storage, buffer, header.location, header.len_buffer);
             // set len_buffer to ret - will be num bytes written or 0 for EOF
             header_out.len_buffer = ret;
-
+            
             // write to send back to remote
             write(fd_out, &header_out, sizeof(HEADER));
         }
         else    // header.type == SHUTDOWN
         {
+            // send acknowledge message
+            header_out.type = ACKNOWLEDGE;
+            header_out.len_message = 0;
+            header_out.location = -1;
+            header_out.len_buffer = -1;
+            write(fd_out, &header_out, sizeof(HEADER));
+            
             sleep(1);           // sleep for 1 second
             close(fd_in);       // close the named pipes
             close(fd_out);
             close_storage(storage);     // close the storage
-
         }
-
+        
         // We broke out because of a disconnection: clean up
         fprintf(stderr, "Closing connection\n");
         close(fd_in);
